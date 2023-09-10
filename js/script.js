951ddfa5b4c2adee5e74fd2310095fa3d3b0deb0
unknown <ahmedirheem2003@gmail.com>
2023-09-10 21:54:48 +0300

    fix sidebar

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ let typed = new Typed('.typing', {
     typeSpeed: 100,
     BackSpeed: 60
 })
+
+function setActiveSection() {
+    let scrollPos = window.scrollY;
+    let sections = document.querySelectorAll('section[id]')
+    let navItems = document.querySelectorAll('.nav a')
+
+    for (let i = 0; i < sections.length; i++) {
+        const currentSection = sections[i];
+        const sectionId = currentSection.getAttribute('id')
+        const navItem = document.querySelector('.nav-' + sectionId)
+
+        if (currentSection.offsetTop <= scrollPos && currentSection.offsetTop + currentSection.offsetHeight > scrollPos) {
+            navItems.forEach(item => {
+                item.classList.remove('active')
+            })
+            navItem.classList.add('active')
+        }
+    }
+}
+window.addEventListener('scroll', setActiveSection)
